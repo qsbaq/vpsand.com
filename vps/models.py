ea26599f2b8f824071ccfc3d101632a1e21551e2
J <jerry.jee@live.com>
2020-06-23 15:16:32 +0800

    init

--- vps/models.py
+++ vps/models.py
@@ -44,7 +44,7 @@ class Goods(models.Model):
         ('Frankfurt','Frankfurt'),
     )
     company = models.ForeignKey(Company , to_field='id' , on_delete=models.DO_NOTHING , verbose_name="商家",blank=False,null=False)
-    pid = models.IntegerField(verbose_name='PID',blank=True,null=False )
+    pid = models.IntegerField(verbose_name='PID',blank=True,null=True )
     dc = models.CharField('机房位置', max_length=256,choices=dcChoices)
     cpu = models.IntegerField('CPU', default=1)
     ram = models.CharField('内存', max_length=256)
