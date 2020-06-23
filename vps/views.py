2ecd9265f1acbd96a5e5503bb343612575b8a7a2
J <jerry.jee@live.com>
2020-06-23 08:37:21 +0800

    init

--- vps/views.py
+++ vps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from vps.models import Company ,Goods
 from django.http import JsonResponse
+from django.contrib import admin
 
 from django.core import serializers
 import json
@@ -11,4 +12,7 @@ def index(request):
     goods = Goods.objects.all().values('pid','company','dc',"cpu",'ram','disk','bandwidth','traffic','route','ipv4','arch','annual','quarter','month','stock','company__name','company__url')
     lastObj = Goods.objects.values('update_time').last()
     update_time = lastObj['update_time'].strftime("%Y-%m-%d %H:%M:%S")
-    return render(request, 'index.html', {'goods': json.dumps(list(goods)) ,'update_time':update_time })
+    return render(request, 'index.html', {'goods': json.dumps(list(goods)) ,
+                                            'update_time':update_time,
+                                            'title':admin.site.site_header ,
+    })
