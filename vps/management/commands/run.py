afac7b40afed76c99a0cdac1a6ebc7e824f38653
unknown <qsbaq@163.com>
2020-06-26 13:14:59 +0800

    fix utf-8

--- vps/management/commands/run.py
+++ vps/management/commands/run.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
         header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
         try:
             req=request.Request(url,headers=header)
-            page=request.urlopen(req,timeout=2).read()
+            page=request.urlopen(req,timeout=2).read().decode('utf-8')
             status = '200'
             if str(page).find(good.company.out_of_stock_string) > 0 :
                 stock = 0    
